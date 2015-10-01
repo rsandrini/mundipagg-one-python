8d9a795c92345869dbf3a4e89bdde3a9decef5c9
nrocha <nrocha@mundipagg.com>
2015-10-01 11:33:43 -0300

    Subindo download de transaction report file

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'mundipaggOnePython')
 setup(
     name='mundipagg_one_python',
     version='1.0.0',
-    package_dir={'mundipaggOnePython': 'mundipaggOnePython', 'data_contracts': 'mundipaggOnePython/data_contracts', 'enum_types': 'mundipaggOnePython/enum_types', 'resource_clients': 'mundipaggOnePython/resource_clients'},
-    packages=['mundipaggOnePython', 'data_contracts', 'enum_types', 'resource_clients'],
+    package_dir={'mundipaggOnePython': 'mundipaggOnePython', 'data_contracts': 'mundipaggOnePython/data_contracts', 'enum_types': 'mundipaggOnePython/enum_types', 'resource_clients': 'mundipaggOnePython/resource_clients', 'transaction_report_file': 'mundipaggOnePython/transaction_report_file'},
+    packages=['mundipaggOnePython', 'data_contracts', 'enum_types', 'resource_clients', 'transaction_report_file'],
     url='https://github.com/mundipagg/mundipagg-one-python',
     license='Apache',
     author='Newton Rocha',
