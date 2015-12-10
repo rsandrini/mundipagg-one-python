7e1f0ea090bddbf8f441c8ac1b0ddafeba966609
Munir Wanis <munirwanis@users.noreply.github.com>
2015-12-10 15:56:48 -0200

    Update AbstractResource.py

--- mundipaggOnePython/resource_clients/AbstractResource.py
+++ mundipaggOnePython/resource_clients/AbstractResource.py
@@ -1,6 +1,6 @@
 from abc import ABCMeta
 
-from mundipaggOnePython import ConfigurationUtility
+from mundipaggOnePython.ConfigurationUtility import ConfigurationUtility
 from enum_types import PlatformEnvironment
 
 
